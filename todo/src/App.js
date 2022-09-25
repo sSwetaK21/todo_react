e14893e3e24553c42d02bedce4ffe42ee9470bd3
shweta <shwetakodamg@gmail.com>
2022-09-25 13:44:13 +0530

    todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,35 +1,45 @@
 import React, { useState } from "react";
 import './App.css';
+import { FcSearch } from "react-icons/fc";
+import { FaEdit } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
+
 
 function App() {
 
 
 
-// const [todo, setTodo] = useState()
+  // const [todo, setTodo] = useState()
 
 
 
   return (
     <div className="App">
       <div className="container">
-        <h1 className="title">Todo List App</h1>
+        <h1 className="title">Todo List App
+        </h1>
 
         <div className="todoBox">
           <form>
             <input type="text"></input>
-            <button type="submit">Go</button>
+            <button type="submit" className="search">        <FcSearch  />
+            </button>
           </form>
 
-        <ul className="ulList">
-          <li className="ullink">
-            <span>Exercise</span>
-            <button type="button" className="editBtn">Edit</button>
-            <button type="button" className="delBtn">Delete</button>
-          </li>
-        </ul>
+          <ul className="ulList">
+            <li className="ullink">
+              <span>Exercise</span>
+              <button type="button" className="editBtn">
+                <FaEdit />
+              </button>
+              <button type="button" className="delBtn">
+                <FaTrashAlt/>
+              </button>
+            </li>
+          </ul>
 
         </div>
-    
+
       </div>
     </div>
   );
